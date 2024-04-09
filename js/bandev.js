01f8a064d8775cf144f34e256abfcaa2c0030597
muHe-blog <jingweihang0824@126.com>
2024-04-09 15:13:11 +0800

    Site updated: 2024-04-09 15:13:11

--- js/bandev.js
+++ js/bandev.js
@@ -1,29 +1,28 @@
 //禁止右键
-document.oncontextmenu = new Function("event.returnValue=false;") //禁用右键
+document.oncontextmenu = function(event) {  
+    event.preventDefault();  
+    return false;  
+};
 
 // 监听按键
 document.onkeydown = function () {
     // f12
     if (window.event && window.event.keyCode == 123) {
-        alert("F12被禁用");
         event.keyCode = 0;
         event.returnValue = false;
     }
     // ctrl+shift+i
     if (window.event.ctrlKey && window.event.shiftKey && window.event.keyCode == 73) {
-        alert("ctrl+shift+i被禁用");
         event.keyCode = 0;
         event.returnValue = false;
     }
     // ctrl+s
     if (window.event.ctrlKey && window.event.keyCode == 83) {
-        alert("ctrl+s被禁用");
         event.keyCode = 0;
         event.returnValue = false;
     }
      // ctrl+p
     if (window.event.ctrlKey && window.event.keyCode == 80) {
-        alert("ctrl+p被禁用");
         event.keyCode = 0;
         event.returnValue = false;
     }
@@ -31,7 +30,7 @@ document.onkeydown = function () {
 
 //禁止先打开控制台然后强行进入
 function a() {
-	// 这里我做的是跳转首页，可以根据实际需要进行其他限制
+    alert("请关闭控制台后再访问本站！！！")
     var domain = window.location.host;// 获取当前域名
     window.location.href = "https://" + domain;
     return false;
